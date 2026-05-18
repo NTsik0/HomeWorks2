a42368e54a3f73a3718ff8e4d9d18075d93957bb
NTsik0 <tsikaridze.nikoloz2006@gmal.com>
2026-05-19 01:48:31 +0400

    Homework 2

---
+++ Homeworks/HomeWork2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Homework2Friend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Nikoloz Tskiaridze"); 
+
+            // Input
+            Console.Write("Input text: ");
+            string inputData = Console.ReadLine();
+
+            Console.WriteLine("Output:");
+            Console.WriteLine(inputData);
+        }
+    }
+}
